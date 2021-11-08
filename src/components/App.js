1e5650342d6d0cd36ff3e485a5da1354c24d7319
Will Jones <willjones484@gmail.com>
2021-11-08 20:57:28 +1100

    aliased edit

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import CreateLink from "./CreateLink";
+import LinkList from "./LinkList";
+
+const App = () => {
+  return <CreateLink />;
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/index.css";
+import App from "./components/App";
 
+// imports from Apollo Client
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+// create http connection to graphql backend
+const httpLink = createHttpLink({
+  uri: "http://localhost:4000",
+});
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMsImlhdCI6MTYzNjM0NjU1Nn0.3z3ywji-JBUfCb9TIgqT5jo7LWtFnC6Uiql1GTIg_iU",
+    },
+  };
+});
+// create graphql client
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+// wrap application in higher-order component ApolloProvider with client as prop
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ApolloProvider>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
